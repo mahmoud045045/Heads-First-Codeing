a3ab16f80bb62dec3aa2d412ea27fe9a751f99f3
Mahmoud <mahmoudsamy045@gmail.com>
2021-08-22 03:58:26 -0700

    code for page 13

--- Chapter1/page13.py
+++ Chapter1/page13.py
@@ -4,3 +4,15 @@ def hook_fish():
 def wait():
     print('waiting...')
     
+print('get worm')
+print('put worm on hook')
+print('throw in lure')
+
+while True:
+     response=input('is bobber underwater?')
+     if response =='yes':
+         is_moving=True
+         print('i got a bite!')
+         hook_fish()
+     else:
+         wait()
